1c7bc6ae8eb64acbce64bb334ee0de67b1292638
Sergey Tyshchenko <113509270+Sergey-Tyshchenko@users.noreply.github.com>
2022-12-23 16:48:21 +0500

    Update Send-MailMessage-Error.ps1

--- Send-MailMessage-Error.ps1
+++ Send-MailMessage-Error.ps1
@@ -1,10 +1,10 @@
 $HName = [System.Net.Dns]::GetHostEntry([string]”localhost”).HostName
-$EmailFrom = ‘o019908@oblgaz56.ru‘
-$EmailTo = ‘o019908@oblgaz56.ru‘
+$EmailFrom = ‘mail8@mail.ru‘
+$EmailTo = ‘mail8@mail.ru‘
 $Subject = "Error!!! Local backup on $HName ended with error!"
 $Body = (‘Backup not complete server’, $HName)
 $SMTPServer = ‘192.168.1.172‘
 $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 25)
 $SMTPClient.EnableSsl = $false
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential(‘o019908@oblgaz56.ru‘, ‘hj9k5g‘);
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential(‘mail8@mail.ru‘, ‘hj9k5g‘);
 $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
